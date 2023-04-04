6ed9861df0d7111e2058a85ce7fafea946b6654f
@khalilullah.alfaath21 <khalilullah.alfaath21@gmail.com>
2023-04-04 09:32:16 +0700

    fix corousel

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,20 @@
 $(document).ready(function(){
     var windowWidth = $(window).width();
-    if (windowWidth < 700){
+    if (windowWidth < 768){
         console.log(screen.width)
         $('nav').addClass('scrolled');
         return;
     }else{
         $('.nav-link').addClass('white-text');
-        $('.navbar-text').addClass('white-text');
+
         $(window).scroll(function() {
                 if ($(this).scrollTop() > 0) {
                     $('nav').addClass('scrolled');
                     $('.nav-link').removeClass('white-text');
-                    $('.navbar-text').removeClass('white-text');
                   } else {
                     $('nav').removeClass('scrolled');
                     $('.nav-link').addClass('white-text');
-                    $('.navbar-text').addClass('white-text');
+            
                   }
             
     
