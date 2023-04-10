33d22f5da717232eb0650e5dbd3bfdebe0d3b1cd
@khalilullah.alfaath21 <khalilullah.alfaath21@gmail.com>
2023-04-11 00:27:17 +0800

    idk what's the update

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,28 @@
 $(document).ready(function () {
   // save 
   var windowWidth = $(window).width();
-  if (windowWidth < 768) {
+  if (windowWidth < 992) {
     console.log(screen.width)
-    $('nav').addClass('scrolled');
+    $('nav').addClass('scrolled box-shadow');
   } else {
     $('.nav-link').addClass('white-text');
 
     $(window).scroll(function () {
       // save last scroll top so when page is refreshed the navbar wont changed to transparent if scrolled
+
       localStorage.setItem("location", $(this).scrollTop());
       if (localStorage.getItem("location") > 0) {
-        $('nav').addClass('scrolled');
+        $('nav').addClass('scrolled box-shadow');
         $('.nav-link').removeClass('white-text');
       } else {
-        $('nav').removeClass('scrolled');
+        $('nav').removeClass('scrolled box-shadow');
         $('.nav-link').addClass('white-text');
 
       }
 
 
     });
+  
   }
 
-
-
-
 });
-
-
-
-
