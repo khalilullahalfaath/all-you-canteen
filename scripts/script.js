2447029db88e2aa9686859fed06a7dec565f2843
@khalilullah.alfaath21 <khalilullah.alfaath21@gmail.com>
2023-04-11 20:15:40 +0800

    final navbar changes javascript code

--- scripts/script.js
+++ scripts/script.js
@@ -2,27 +2,32 @@ $(document).ready(function () {
   // save 
   var windowWidth = $(window).width();
   if (windowWidth < 992) {
-    console.log(screen.width)
     $('nav').addClass('scrolled box-shadow');
   } else {
+    console.log("masuk")
     $('.nav-link').addClass('white-text');
 
+    // get the last scroll position from local storage
+    var lastScrollPos = localStorage.getItem("last scroll position");
+    
+    // check if the last scroll position is greater than 0
+    if (lastScrollPos > 0) {
+      $('nav').addClass('scrolled box-shadow');
+      $('.nav-link').removeClass('white-text');
+    }
+    
     $(window).scroll(function () {
       // save last scroll top so when page is refreshed the navbar wont changed to transparent if scrolled
-
-      localStorage.setItem("location", $(this).scrollTop());
-      if (localStorage.getItem("location") > 0) {
+      var lastPos = window.pageYOffset;
+      localStorage.setItem("last scroll position", lastPos);
+      var lastScrollPos = localStorage.getItem("last scroll position");
+      if (lastScrollPos > 0) {
         $('nav').addClass('scrolled box-shadow');
         $('.nav-link').removeClass('white-text');
       } else {
         $('nav').removeClass('scrolled box-shadow');
         $('.nav-link').addClass('white-text');
-
       }
-
-
     });
-  
   }
-
 });
